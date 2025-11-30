8728facc36f8d79d54d0fb83df141f80f11f1fec
ZDavd <mantillo01@gmail.com>
2025-11-30 17:46:49 +0100

    es 101 completed

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,6 +2,7 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -12,9 +13,6 @@ import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
 
 /**
  * This class will contain four utility functions on lists and maps, of which the first one is provided as example.
@@ -64,7 +62,9 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return emptyList();
+        final List<Optional<T>> l = new ArrayList<>(list.size());
+        list.forEach(el -> l.add(Optional.ofNullable(el).filter(pre)));
+        return l;
     }
 
     /**
@@ -83,7 +83,15 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return emptyMap();
+        final Map<R, Set<T>> m = new HashMap<>();
+        list.forEach(el -> {
+            final R key = op.apply(el);
+            m.merge(key, new HashSet<>(Set.of(el)), (existingSet, newSet) -> {
+                existingSet.addAll(newSet);
+                return existingSet;
+            });
+        });
+        return m;g
     }
 
     /**
@@ -104,7 +112,11 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return emptyMap();
+        final Map<K, V> newMap = new HashMap<>();
+        map.forEach((k, v) -> {
+            newMap.put(k, v.orElse(def.get()));
+        });
+        return newMap;
     }
 
     /**
